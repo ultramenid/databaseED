cfa3c3ac652c74cfc49b52d89497ecf29530a994
ultramenid <malichamdan@gmail.com>
2022-06-07 22:32:51 +0700

    icon css

--- app/Http/Livewire/MapDatabase.php
+++ app/Http/Livewire/MapDatabase.php
@@ -8,21 +8,12 @@ use Livewire\Component;
 class MapDatabase extends Component
 {
     public function getMarker(){
-        $bExecution =  DB::table('eddatabase')->select('kasus','lat', 'long','tanggalkejadian','bentukancaman','korban', 'pelaku')->get();
-        foreach($bExecution as $item){
-            $data['kasus'][] = $item->kasus;
-            $data['lat'][] = $item->lat;
-            $data['long'][] = $item->long;
-            $data['tanggalkejadian'][] = $item->tanggalkejadian;
-            $data['bentukancaman'][] = $item->bentukancaman;
-            $data['korban'][] = $item->korban;
-            $data['pelaku'][] = $item->pelaku;
-        }
-        // dd($data);
-        return json_encode($data);
+        return  DB::table('eddatabase')->select('kasus','lat', 'long','tanggalkejadian','bentukancaman','korban', 'pelaku')->get();
+
     }
     public function render()
     {
-        return view('livewire.map-database');
+        $markers = $this->getMarker();
+        return view('livewire.map-database', compact('markers'));
     }
 }
