baefd1a49a1fa9fa2a707f2aea96d74e27466d3b
ultramenid <malichamdan@gmail.com>
2022-06-17 22:26:10 +0700

    popup change tanggal to akibat

--- resources/views/livewire/map-database.blade.php
+++ resources/views/livewire/map-database.blade.php
@@ -15,7 +15,7 @@
             var marker = L.marker(new L.LatLng({{$item->lat}}, {{$item->long}}), { title: "{{$item->kasus}}"});
             marker.bindPopup('<div class="flex flex-col text-black w-full">'+
                 ' <h1 class="text-xl font-semibold">{{$item->kasus}}</h1>'+
-                    '<div class="mt-4 flex space-x-2"><a style="color:black" class="font-semibold">Tanggal Kejadian:</a> <a style="color:black"> {{ \Carbon\Carbon::parse($item->tanggalkejadian)->format("d M Y")}}</a></div>'+
+                    '<div class="mt-4 flex space-x-2"><a style="color:black" class="font-semibold"> Akibat:</a> <a style="color:black"> {{$item->akibat}}</a></div>'+
 
                     '<div class=" flex space-x-2"><a style="color:black" class="font-semibold">Tindakan:</a> <a style="color:black"> {{$item->bentukancaman}}</a></div>'+
                     '<div class="flex space-x-2">'+
