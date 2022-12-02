c8dfae16e9a8a5c37e124bb0b371771dcf157dec
ultramenid <malichamdan@gmail.com>
2022-12-02 12:53:37 +0700

    hidden null namapelaku

--- resources/views/livewire/map-database.blade.php
+++ resources/views/livewire/map-database.blade.php
@@ -43,29 +43,52 @@
                     pointToLayer: function(feature){
                         // console.log(feature)
                         var marker = L.marker([feature.properties.lat, feature.properties.long]);
-                        marker.bindPopup('<div class="flex flex-col text-black w-full">'+
-                        ' <h1 class="text-xl font-semibold">'+feature.properties.kasus+'</h1>'+
-                            '<div class="mt-4 flex space-x-2"><a style="color:black" class="font-semibold"> Akibat:</a> <a style="color:black"> '+feature.properties.akibat+'</a></div>'+
+                        if(!feature.properties.namapelaku){
+                                marker.bindPopup('<div class="flex flex-col text-black w-full">'+
+                                ' <h1 class="text-xl font-semibold">'+feature.properties.kasus+'</h1>'+
+                                    '<div class="mt-4 flex space-x-2"><a style="color:black" class="font-semibold"> Akibat:</a> <a style="color:black"> '+feature.properties.akibat+'</a></div>'+
 
-                            '<div class=" flex space-x-2"><a style="color:black" class="font-semibold">Tindakan:</a> <a style="color:black">'+feature.properties.bentukancaman+'</a></div>'+
-                            '<div class="flex space-x-2">'+
-                            '<a style="color:black" class="font-semibold">Pelaku:</a> <a style="color:black">'+feature.properties.pelaku+'</a>'+
-                            '</div>'+
-                                                '<div class="flex space-x-2">'+
-                            '<a style="color:black" class="font-semibold">Nama Pelaku:</a> <a style="color:black">'+feature.properties.namapelaku+'</a>'+
-                            '</div>'+
-                                                '<div class="flex space-x-2">'+
-                                '<a style="color:black" class="font-semibold">Jumlah Korban: </a> <a style="color:black">'+feature.properties.jumlahkorban+'</a>' +
-                            '</div>'+
-                            '<div class="flex space-x-2">'+
-                                '<a style="color:black" class="font-semibold">Konflik Dengan: </a> <a style="color:black">'+feature.properties.konflikdengan+'</a>'+
-                            '</div>'+
-                            '<div class="flex space-x-2">'+
-                                '<a style="color:black" class="font-semibold">Sektor: </a> <a style="color:black">'+feature.properties.sektor+'</a>'+
-                            '</div>'+
-                            '</div>'+
-                        '</div>');
-                        return marker;
+                                    '<div class=" flex space-x-2"><a style="color:black" class="font-semibold">Tindakan:</a> <a style="color:black">'+feature.properties.bentukancaman+'</a></div>'+
+                                    '<div class="flex space-x-2">'+
+                                    '<a style="color:black" class="font-semibold">Pelaku:</a> <a style="color:black">'+feature.properties.pelaku+'</a>'+
+                                    '</div>'+
+                                                        '<div class="flex space-x-2">'+
+                                        '<a style="color:black" class="font-semibold">Jumlah Korban: </a> <a style="color:black">'+feature.properties.jumlahkorban+'</a>' +
+                                    '</div>'+
+                                    '<div class="flex space-x-2">'+
+                                        '<a style="color:black" class="font-semibold">Konflik Dengan: </a> <a style="color:black">'+feature.properties.konflikdengan+'</a>'+
+                                    '</div>'+
+                                    '<div class="flex space-x-2">'+
+                                        '<a style="color:black" class="font-semibold">Sektor: </a> <a style="color:black">'+feature.properties.sektor+'</a>'+
+                                    '</div>'+
+                                    '</div>'+
+                                '</div>');
+                            }else{
+                                marker.bindPopup('<div class="flex flex-col text-black w-full">'+
+                                ' <h1 class="text-xl font-semibold">'+feature.properties.kasus+'</h1>'+
+                                    '<div class="mt-4 flex space-x-2"><a style="color:black" class="font-semibold"> Akibat:</a> <a style="color:black"> '+feature.properties.akibat+'</a></div>'+
+
+                                    '<div class=" flex space-x-2"><a style="color:black" class="font-semibold">Tindakan:</a> <a style="color:black">'+feature.properties.bentukancaman+'</a></div>'+
+                                    '<div class="flex space-x-2">'+
+                                    '<a style="color:black" class="font-semibold">Pelaku:</a> <a style="color:black">'+feature.properties.pelaku+'</a>'+
+                                    '</div>'+
+                                                        '<div class="flex space-x-2">'+
+                                    '<a style="color:black" class="font-semibold">Nama Pelaku:</a> <a style="color:black">'+feature.properties.namapelaku+'</a>'+
+                                    '</div>'+
+                                                        '<div class="flex space-x-2">'+
+                                        '<a style="color:black" class="font-semibold">Jumlah Korban: </a> <a style="color:black">'+feature.properties.jumlahkorban+'</a>' +
+                                    '</div>'+
+                                    '<div class="flex space-x-2">'+
+                                        '<a style="color:black" class="font-semibold">Konflik Dengan: </a> <a style="color:black">'+feature.properties.konflikdengan+'</a>'+
+                                    '</div>'+
+                                    '<div class="flex space-x-2">'+
+                                        '<a style="color:black" class="font-semibold">Sektor: </a> <a style="color:black">'+feature.properties.sektor+'</a>'+
+                                    '</div>'+
+                                    '</div>'+
+                                '</div>');
+                            }
+
+                            return marker;
                     }
                 })
                 map.addLayer(markers);
