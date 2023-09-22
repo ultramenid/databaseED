2bc0ce194686c5ce419daeb888c7d14903e3a549
ultramenid <malichamdan@gmail.com>
2023-09-22 13:03:41 +0700

    nav overflow

--- resources/views/partials/backendNav.blade.php
+++ resources/views/partials/backendNav.blade.php
@@ -1,18 +1,19 @@
 <div class="border-b border-gray-300 sticky top-0 z-40 bg-white ">
     <div class="max-w-5xl mx-auto px-4"  x-data="{ pages: false }">
-        <nav class=" flex space-x-6 sm:justify-start justify-center text-sm leading-5 overflow-x-auto flex-wrap scrollbar-hide text-gray-500">
-            <div class="hover:bg-gray-200  hover:px-2 py-3 rounded @if($nav == 'kasus' )border-b-2   border-newgray-900 @endif ">
+        <nav class=" flex space-x-6 text-sm leading-5 overflow-x-auto scrollbar-hide">
+            <div class="whitespace-nowrap hover:bg-gray-200 flex flex-nowrap  hover:px-2 py-3 rounded w-full @if($nav == 'kasus' )border-b-2   border-newgray-900 @endif ">
                 <a href="{{url('/cms/dashboard')}}" class=" px-0.5  @if($nav == 'kasus' )   text-black  @endif    cursor-pointer" >Database Kasus</a>
             </div>
 
-            <div class="hover:bg-gray-200  hover:px-2 py-3 rounded @if($nav == 'ahli' )border-b-2   border-newgray-900 @endif ">
+            <div class="whitespace-nowrap hover:bg-gray-200 flex flex-nowrap  hover:px-2 py-3 rounded w-full @if($nav == 'ahli' )border-b-2   border-newgray-900 @endif ">
                 <a href="{{url('/cms/dbahli')}}" class=" px-0.5  @if($nav == 'ahli' )   text-black  @endif    cursor-pointer" >Database Ahli</a>
             </div>
 
-            <div class="hover:bg-gray-200  hover:px-2 py-3 rounded @if($nav == 'sda' )border-b-2   border-newgray-900 @endif ">
+            <div class="whitespace-nowrap hover:bg-gray-200 flex flex-nowrap  hover:px-2 py-3 rounded w-full @if($nav == 'sda' )border-b-2   border-newgray-900 @endif ">
                 <a href="{{url('/cms/dbsda')}}" class=" px-0.5  @if($nav == 'sda' )   text-black  @endif    cursor-pointer" >Database Kasus SDA</a>
             </div>
-
         </nav>
     </div>
 </div>
+
+
